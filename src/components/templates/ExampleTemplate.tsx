f2ab4a46ea94bb8ac87e51e4d068865b9da439f9
sodavinchheng-quad <sodavin.chheng@quad.co.jp>
2025-02-05 12:37:31 +0900

    [AI] 2025-02-05 12:37:31.468471: Add example styling

--- src/components/templates/ExampleTemplate.tsx
+++ src/components/templates/ExampleTemplate.tsx
@@ -1,13 +1,28 @@
+import styled from "styled-components";
+
 type Props = {
   children: React.ReactNode;
 };
 
 export const ExampleTemplate: React.FC<Props> = ({ children }) => {
   return (
-    <div>
-      <p>Example Template</p>
+    <StyledTemplateContainer>
+      <StyledExampleTitle>Example Template</StyledExampleTitle>
 
       {children}
-    </div>
+    </StyledTemplateContainer>
   );
 };
+
+const StyledTemplateContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 16px;
+`;
+
+const StyledExampleTitle = styled.div`
+  font-size: 16px;
+  font-weight: bold;
+`;
